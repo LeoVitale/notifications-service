7b65596208134c9e72b40236d6ead4879245061b
Leonardo Vitale <leonardo.vitale@xelix.com>
2022-12-15 02:13:57 +0000

    define prisma services and create first api calls

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notification-body';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(
+    @Body()
+    { recipientId, content, category }: CreateNotificationBody,
+  ) {
+    return this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        category,
+        content,
+        recipientId,
+      },
+    });
   }
 }
