9534601537e470211dfa5a01d2d810689837d47b
Leonardo Vitale <leonardo.vitale@outlook.com>
2022-12-16 22:36:42 +0000

    create prisma notification mapper

--- src/infra/database/prisma/repositories/prisma-notification-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -2,21 +2,16 @@ import { NotificationsRepository } from '@application/repositories/notification-
 import { Injectable } from '@nestjs/common';
 import { Notification } from '@application/entities/notification';
 import { PrismaService } from '../prisma.service';
+import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper';
 
 @Injectable()
 export class PrismaNotificationRepository implements NotificationsRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(notification: Notification) {
+    const raw = PrismaNotificationMapper.toPersistence(notification);
     await this.prismaService.notification.create({
-      data: {
-        id: notification.id,
-        content: notification.content.value,
-        category: notification.category,
-        recipientId: notification.recipientId,
-        readAt: notification.readAt,
-        createdAt: notification.createdAt,
-      },
+      data: raw,
     });
   }
 }
