8f9b34fc89f08fbdfd7a92891cdb355125df5e7a
Leonardo Vitale <leonardo.vitale@outlook.com>
2022-12-16 17:46:47 +0000

    create prisma repository

---
+++ src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -0,0 +1,20 @@
+import { Notification } from 'src/application/entities/notification';
+import { NotificationsRepository } from '../../../../application/repositories/notification-repository';
+import { PrismaService } from '../prisma.service';
+
+export class PrismaNotificationRepository implements NotificationsRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(notification: Notification) {
+    await this.prismaService.notification.create({
+      data: {
+        id: notification.id,
+        content: notification.content.value,
+        category: notification.category,
+        recipientId: notification.recipientId,
+        readAt: notification.readAt,
+        createdAt: notification.createdAt,
+      },
+    });
+  }
+}
