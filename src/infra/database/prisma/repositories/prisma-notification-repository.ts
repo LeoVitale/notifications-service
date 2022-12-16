674cb75aaabdeb713565756023e785df6a5d8a6e
Leonardo Vitale <leonardo.vitale@outlook.com>
2022-12-16 18:43:48 +0000

    fix prisma notification repository

--- src/infra/database/prisma/repositories/prisma-notification-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { Notification } from 'src/application/entities/notification';
 import { NotificationsRepository } from '../../../../application/repositories/notification-repository';
 import { PrismaService } from '../prisma.service';
 
+@Injectable()
 export class PrismaNotificationRepository implements NotificationsRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
